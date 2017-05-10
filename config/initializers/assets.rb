b170ebb023a1689b5683e565f1ff4a38a6d515c2
drisclouds90 <idris@galaksi.mobi>
2017-05-10 18:21:55 +0700

    configure css precompile

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -10,5 +10,10 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
+
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
+
+# Front Website
+Rails.application.config.assets.precompile += %w( front/welcome.css )
+Rails.application.config.assets.precompile += %w( front/style.css )
