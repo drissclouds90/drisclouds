ad6bb26f48aa841c36701310eef43011dfec2143
drisclouds90 <idris@galaksi.mobi>
2017-05-10 15:52:58 +0700

    modified title

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+
+  get 'welcome/index'
   get 'welcome/home'
   get 'welcome/help'
   get 'welcome/about'
 
-  
+
+  root 'welcome#index'
 
 end
--- test/controllers/welcome_controller_test.rb
+++ test/controllers/welcome_controller_test.rb
@@ -2,19 +2,34 @@ require 'test_helper'
 
 class WelcomeControllerTest < ActionDispatch::IntegrationTest
 
+  def setup
+    @base_title = "Damn on Rails"
+  end
+
+
+  test "should get index" do
+      get welcome_index_url
+      assert_response :success
+      assert_select "title", "Welcome | Damn on Rails"
+  end
+
   test "should get home" do
     get welcome_home_url
     assert_response :success
+    assert_select "title", "Home | Damn on Rails"
   end
 
   test "should get help" do
     get welcome_help_url
     assert_response :success
+    assert_select "title", "Help | Damn on Rails"
   end
 
   test "should get about" do
     get welcome_about_url
     assert_response :success
+    assert_select "title", "About | Damn on Rails"
   end
+
 
 end
